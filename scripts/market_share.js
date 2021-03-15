68c632862215217be9edcc658b155d29c8ca2f2e
sami061190 <sami061190@gmail.com>
2021-03-15 14:00:38 +0100

    Added color blind setting to market share graphs

--- scripts/market_share.js
+++ scripts/market_share.js
@@ -2,6 +2,7 @@ var colorPalette = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911
 //var colorPalette = ["#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059", "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87", "#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80", "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100", "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F", "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09", "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66", "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C", "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81", "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00", "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700", "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329", "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C"]
 
 var charts = [];
+var amCharts = [];
 var industryCharts = [];
 var toolTip = {
     shared: false
@@ -28,6 +29,13 @@ var industryPercentChartData = [];
 var marketCapChartData = [];
 var marketCapPercentChartData = [];
 
+var volumeAmChartData = [];
+var percentAmChartData = [];
+var marketCapAmChartData = [];
+var marketCapPercentAmChartData = [];
+
+
+
 var marketCapChartOptions = {
     animationEnabled: true,
     theme: "light2",
@@ -35,6 +43,7 @@ var marketCapChartOptions = {
         text: "Market Cap By Sector"
     },
     axisX: {
+        title: "Date",
         stripLines: [
             {
                 value: new Date(2019, 11, 31),
@@ -68,6 +77,7 @@ var marketCapPercentChartOptions = {
         text: "Market Share By Sector"
     },
     axisX: {
+        title: "Date",
         stripLines: [
             {
                 value: new Date(2019, 11, 31),
@@ -99,6 +109,7 @@ var volumeChartOptions = {
         text: "Transaction Volume By Sector"
     },
     axisX: {
+        title: "Date",
         stripLines: [
             {
                 value: new Date(2019, 11, 31),
@@ -133,6 +144,7 @@ var percentChartOptions = {
         text: "Transaction Percent By Sector"
     },
     axisX: {
+        title: "Date",
         stripLines: [
             {
                 value: new Date(2019, 11, 31),
@@ -168,6 +180,7 @@ var industryVolumeChartOptions = {
         text: sector + ": Transaction Volume By Industry"
     },
     axisX: {
+        title: "Date",
         stripLines: [
             {
                 value: new Date(2019, 11, 31),
@@ -296,6 +309,50 @@ function syncCharts(charts, syncToolTip, syncCrosshair, syncAxisXRange) {
 
 function loadData() {
 
+
+    var patternsList = [];
+    for (var i = 0; i < 35; i++) {
+        var patternTypes = ["RectPattern", "LinePattern"];
+        var random = Math.random();
+        var type = patternTypes[patternsList.length % patternTypes.length];
+        var recSize = Math.round(random * 12) + 1;
+        var shapeSize = recSize + Math.round(random * 10);
+        var rotation = Math.round(90 * random);
+        var gap = random * 5 + 2;
+
+        var pattern = {
+            "type": type,
+            "rectWidth": recSize,
+            "rectHeight": recSize,
+            "width": shapeSize,
+            "height": shapeSize,
+            "fill": colorPalette[i % colorPalette.length],
+            "backgroundFill": colorPalette[i % colorPalette.length],
+            "backgroundOpacity": 0.3,
+            "rotation": rotation,
+            "strokeWidth": 0
+        }
+
+        if (type == "LinePattern") {
+            pattern = {
+                "type": type,
+                "width": 1000,
+                "height": 1000,
+                "gap": gap,
+                "fill": colorPalette[i % colorPalette.length],
+                "backgroundFill": colorPalette[i % colorPalette.length],
+                "backgroundOpacity": 0.3,
+                "stroke": colorPalette[i % colorPalette.length],
+                "rotation": rotation,
+                "strokeWidth": 1
+            }
+        }
+
+        patternsList.push(pattern);
+    }
+
+
+
     var lines = sectorDataTable.split(/[\r\n]+/g);
 
     for (var i = 1; i < lines.length; i++) {
@@ -304,13 +361,19 @@ function loadData() {
         var sector = row[0];
 
         var volumeChartDataPoints = [];
-        var percentChartDataPoints = []
+        var percentChartDataPoints = [];
+        var volumeChartAmDataPoints = [];
+        var percentChartAmDataPoints = [];
+
         for (var column = 0; column < row.length / 2 - 1; column++) {
             var volume = parseFloat((parseFloat(row[2 * column + 1]) / 1000000).toFixed(2));
             var percent = parseFloat(parseFloat(row[2 * column + 2]).toFixed(2));
             var date = new Date(2010 + column, 11, 31);
             volumeChartDataPoints.push({ y: volume, x: date, label: sector });
             percentChartDataPoints.push({ y: percent, x: date, label: sector });
+            volumeChartAmDataPoints.push({ "volume": volume, "date": date });
+            percentChartAmDataPoints.push({ "percent": percent, "date": date });
+
         }
 
         volumeChartData.push(
@@ -329,6 +392,24 @@ function loadData() {
             }
         )
 
+
+
+        volumeAmChartData.push({
+            "type": "LineSeries",
+            "name": sector,
+            "stacked": true,
+            "tooltipText": "Series: {name}\nValue: {valueY}",
+            "stroke": colorPalette[volumeAmChartData.length % colorPalette.length],
+            "strokeOpacity": 1,
+            "fill": patternsList[volumeAmChartData.length % patternsList.length],
+            "fillOpacity": 1,
+            "dataFields": {
+                "valueY": "volume",
+                "dateX": "date"
+            },
+            "data": volumeChartAmDataPoints
+        })
+
         percentChartData.push(
             {
                 type: "stackedArea",
@@ -343,6 +424,27 @@ function loadData() {
             }
         )
 
+        percentAmChartData.push({
+            "type": "LineSeries",
+            "name": sector,
+            "stacked": true,
+            "tooltipText": "Series: {name}\nValue: {valueY}",
+            "stroke": colorPalette[percentAmChartData.length % colorPalette.length],
+            "strokeOpacity": 1,
+            "fill": patternsList[percentAmChartData.length % patternsList.length],
+            "fillOpacity": 1,
+            "dataFields": {
+                "valueY": "percent",
+                "dateX": "date"
+            },
+            "data": percentChartAmDataPoints,
+            "events": {
+                "hit": function (ev) {
+                    console.log("clicked on ", ev.target);
+                }
+            }
+        })
+
     }
 
 
@@ -350,6 +452,68 @@ function loadData() {
     charts.push(new CanvasJS.Chart("sectorPercentContainer", percentChartOptions))
 
 
+
+
+    am4core.useTheme(am4themes_patterns);
+
+    var sectorVolumeAmChart = am4core.createFromConfig({
+
+        "series": volumeAmChartData,
+        "titles": [{
+            "text": "Transaction Volume By Sector",
+            "fontSize": 25,
+            "marginBottom": 0
+          }],
+        "xAxes": [{
+            "type": "DateAxis",
+            "title": {
+                "text": "Date"
+            }
+        }],
+        "yAxes": [{
+            "type": "ValueAxis",
+            "title": {
+                "text": "Transactions (M)"
+            }
+        }],
+        "cursor": {
+            "type": "XYCursor",
+            "maxTooltipDistance": 1
+        }
+
+
+    }, "sectorVolumeAmContainer", am4charts.XYChart);
+
+    var sectorPercentAmChart = am4core.createFromConfig({
+
+        "series": percentAmChartData,
+        "titles": [{
+            "text": "Transaction Percent By Sector",
+            "fontSize": 25,
+            "marginBottom": 0
+          }],
+        "xAxes": [{
+            "type": "DateAxis",
+            "title": {
+                "text": "Date"
+            }
+        }],
+        "yAxes": [{
+            "type": "ValueAxis",
+            "title": {
+                "text": "Percent %"
+            }
+        }],
+        "cursor": {
+            "type": "XYCursor",
+            "maxTooltipDistance": 1
+        },
+
+
+    }, "sectorPercentAmContainer", am4charts.XYChart);
+
+    amCharts.push(sectorVolumeAmChart);
+    amCharts.push(sectorPercentAmChart);
 
     //Load Industries charts
 
@@ -408,6 +572,9 @@ function loadData() {
     var capChartDataPoints = [];
     var percentCapChartDataPoints = []
 
+    var capAmChartDataPoints = [];
+    var percentCapAmChartDataPoints = []
+
     for (var i = 1; i < lines.length + 1; i++) {
         var row = ["no sector", "2020-12-31", "0.0", "0.0"];
         if (i < lines.length) {
@@ -434,6 +601,22 @@ function loadData() {
                 }
             );
 
+            marketCapAmChartData.push({
+                "type": "LineSeries",
+                "name": sector,
+                "stacked": true,
+                "tooltipText": "Series: {name}\nValue: {valueY}",
+                "stroke": colorPalette[marketCapAmChartData.length % colorPalette.length],
+                "strokeOpacity": 1,
+                "fill": patternsList[marketCapAmChartData.length % patternsList.length],
+                "fillOpacity": 1,
+                "dataFields": {
+                    "valueY": "value",
+                    "dateX": "date"
+                },
+                "data": capAmChartDataPoints
+            });
+
             marketCapPercentChartData.push(
                 {
                     type: "stackedArea",
@@ -446,16 +629,36 @@ function loadData() {
                     //yValueFormatString: "#.### %",
                     legendMarkerType: "square"
                 }
-            )
+            );
+
+            marketCapPercentAmChartData.push({
+                "type": "LineSeries",
+                "name": sector,
+                "stacked": true,
+                "tooltipText": "Series: {name}\nValue: {valueY}",
+                "stroke": colorPalette[marketCapPercentAmChartData.length % colorPalette.length],
+                "strokeOpacity": 1,
+                "fill": patternsList[marketCapPercentAmChartData.length % patternsList.length],
+                "fillOpacity": 1,
+                "dataFields": {
+                    "valueY": "percent",
+                    "dateX": "date"
+                },
+                "data": percentCapAmChartDataPoints
+            });
 
             sector = row[0]
             capChartDataPoints = [];
             percentCapChartDataPoints = []
+            capAmChartDataPoints = []
+            percentCapAmChartDataPoints = []
 
         }
 
         capChartDataPoints.push({ y: cap, x: date, label: row[0] });
         percentCapChartDataPoints.push({ y: percent, x: date, label: row[0] });
+        capAmChartDataPoints.push({ "value": cap, "date": date });
+        percentCapAmChartDataPoints.push({ "percent": percent, "date": date });
 
     }
 
@@ -468,6 +671,66 @@ function loadData() {
         charts[i].render();
     }
 
+
+    var sectorVolumeAmChart = am4core.createFromConfig({
+
+        "series": marketCapAmChartData,
+        "titles": [{
+            "text": "Market Cap By Sector",
+            "fontSize": 25,
+            "marginTop": 0,
+            "marginBottom": 0
+          }],
+        "xAxes": [{
+            "type": "DateAxis",
+            "title": {
+                "text": "Date"
+            }
+        }],
+        "yAxes": [{
+            "type": "ValueAxis",
+            "title": {
+                "text": "Trillions USD"
+            }
+        }],
+        "cursor": {
+            "type": "XYCursor",
+            "maxTooltipDistance": 1
+        },
+
+
+    }, "sectorMarketCapAmContainer", am4charts.XYChart);
+
+    var sectorCapPercentAmChart = am4core.createFromConfig({
+
+        "series": marketCapPercentAmChartData,
+        "titles": [{
+            "text": "Market Share By Sector",
+            "fontSize": 25,
+            "marginBottom": 0
+          }],
+        "xAxes": [{
+            "type": "DateAxis",
+            "title": {
+                "text": "Date"
+            }
+        }],
+        "yAxes": [{
+            "type": "ValueAxis",
+            "title": {
+                "text": "Percent %"
+            }
+        }],
+        "cursor": {
+            "type": "XYCursor",
+            "maxTooltipDistance": 1
+        },
+
+
+    }, "sectorMarketCapPercentAmContainer", am4charts.XYChart);
+
+    amCharts.push(sectorVolumeAmChart);
+    amCharts.push(sectorCapPercentAmChart);
 
     industryCharts.push(new CanvasJS.Chart("industryVolumeContainer", industryVolumeChartOptions))
     industryCharts.push(new CanvasJS.Chart("industryPercentContainer", industryPercentChartOptions))
@@ -494,6 +757,32 @@ function sectorCheckBoxChanged(dataSeriesIndex, checkBoxObject) {
         charts[i].data[dataSeriesIndex].set("visible", isEnabled, true);
     }
 
+    for (var i = 0; i < amCharts.length; i++) {
+        //charts[i].data[dataSeriesIndex].set("visible", isEnabled, true);
+        if (isEnabled) {
+            amCharts[i].series.getIndex(dataSeriesIndex).show()
+        } else {
+            amCharts[i].series.getIndex(dataSeriesIndex).hide()
+        }
+    }
+
+
+}
+
+function colorBlindSwitchToggled(switchObject) {
+    var isEnabled = switchObject.checked;
+
+    if (isEnabled) {
+        document.getElementById("canvas-charts").style.display = "none"
+        document.getElementById("am-charts").style.display = "block"
+    } else {
+        document.getElementById("canvas-charts").style.display = "block"
+        document.getElementById("am-charts").style.display = "none"
+
+        for (var i = 0; i < charts.length; i++) {
+            charts[i].render();
+        }
+    }
 
 }
 
